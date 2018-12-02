a906160755ace438fd7ba7d5b210ff2750b885e7
Danilo Fernandes <DaniloFnds@users.noreply.github.com>
2018-12-02 02:43:26 -0200

    alteracao no orgin

--- src/main/java/br/com/danilo/timework/controller/TimerController.java
+++ src/main/java/br/com/danilo/timework/controller/TimerController.java
@@ -9,7 +9,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://timerwork.herokuapp.com/")
 @RestController
 @RequestMapping
 public class TimerController {
