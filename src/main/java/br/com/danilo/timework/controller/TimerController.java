e105d25b6e518c2e1223b517666edd8c6a6c5fb5
Danilo Fernandes <DaniloFnds@users.noreply.github.com>
2018-12-02 02:56:07 -0200

    asdas

--- src/main/java/br/com/danilo/timework/controller/TimerController.java
+++ src/main/java/br/com/danilo/timework/controller/TimerController.java
@@ -9,7 +9,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://timerwork.herokuapp.com/")
+@CrossOrigin
 @RestController
 @RequestMapping
 public class TimerController {
